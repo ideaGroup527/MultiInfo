b6ed8cddf229ddefa3f04c59b16e1b8999929efa
Absurd <www_1350@163.com>
2015-11-24 17:04:15 +0800

    添加公有produces头

---
+++ src/main/java/org/jmu/multiinfo/utils/MediaTypes.java
@@ -0,0 +1,34 @@
+package org.jmu.multiinfo.utils;
+
+
+/***
+ * 
+* @Title: MediaTypes.java 
+* @Package org.jmu.multiinfo.utils 
+* @Description: 带UTF-8 charset 定义的MediaType.
+* @author  <a href="mailto:www_1350@163.com">Absurd</a>
+* @date 2015年11月23日 下午8:42:14 
+* @version V1.0
+ */
+public class MediaTypes {
+	public static final String APPLICATION_XML = "application/xml";
+	public static final String APPLICATION_XML_UTF_8 = "application/xml; charset=UTF-8";
+
+	public static final String JSON = "application/json";
+	public static final String JSON_UTF_8 = "application/json; charset=UTF-8";
+
+	public static final String JAVASCRIPT = "application/javascript";
+	public static final String JAVASCRIPT_UTF_8 = "application/javascript; charset=UTF-8";
+
+	public static final String APPLICATION_XHTML_XML = "application/xhtml+xml";
+	public static final String APPLICATION_XHTML_XML_UTF_8 = "application/xhtml+xml; charset=UTF-8";
+
+	public static final String TEXT_PLAIN = "text/plain";
+	public static final String TEXT_PLAIN_UTF_8 = "text/plain; charset=UTF-8";
+
+	public static final String TEXT_XML = "text/xml";
+	public static final String TEXT_XML_UTF_8 = "text/xml; charset=UTF-8";
+
+	public static final String TEXT_HTML = "text/html";
+	public static final String TEXT_HTML_UTF_8 = "text/html; charset=UTF-8";
+}
